dc83cd14501793194ffeac2d1d229719ea4e84a7
Rafael Silva <rafaelssilva335@gmail.com>
2023-05-02 09:07:25 -0300

    feat: adiciona controller de pacientes, e rotas de API para pacientes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/patients', 'PatientController@index');
+Route::post('/patients', 'PatientController@store');
+Route::get('/patients/{id}', 'PatientController@show');
+Route::put('/patients/{id}', 'PatientController@update');
+Route::delete('/patients/{id}', 'PatientController@destroy');
